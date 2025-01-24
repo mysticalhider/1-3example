20fff90d92d1002f09146b4ab9511bc47cbf7c9b
Perrin Dalgleish <perrin.dalgleish@gmail.com>
2025-01-24 10:18:53 -0500

    Add project files.

---
+++ 1-3example/1-3example.cpp
@@ -0,0 +1,28 @@
+/*
+File Name: 1-3example.cpp
+Programmer: Perrin Dalgleish
+Date: 1/24/25
+Requirements:
+	obtain hours worked for the week
+	ask pay rate
+	compute gross pay
+	display it
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int hoursworked;
+	int payrate;
+	int grosspay;
+	cout << "How many hours did you work this week? \n";
+	cin >> hoursworked;
+	cout << "How much do you make per hour? \n";
+	cin >> payrate;
+	grosspay = hoursworked * payrate;
+	cout << "You worked " << hoursworked << " hours this week while your hourly rate is $" << payrate << ". \n";
+	cout << "You made $" << grosspay << " this week. WOW!";
+}
